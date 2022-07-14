e7e23f28b40c97895d39301146d3317145664ab1
Sergii Butenko <sergii.butenko@globallogic.com>
2022-07-14 10:42:35 +0200

    fixed merge conflict

--- app.py
+++ app.py
@@ -19,17 +19,6 @@ test#6
 
 test#7
 
-<<<<<<< HEAD
 test#8
 
 test#9
-=======
-<<<<<<< HEAD
-test#8
-
-test#9
-=======
-
-test#8
->>>>>>> 2e71439041ad839bfb70b2faab2a0cef48d91fa2
->>>>>>> dd33eee53eaa7859aaa1834ac970a20eec038c23
